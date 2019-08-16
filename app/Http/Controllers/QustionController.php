99cca86ce08e6f937d0ca0a8bfd429d57f7b2aec
antonblue16 <anton.hartanto60@gmail.com>
2019-08-16 20:42:39 +0700

    Display All Question and Pagination

---
+++ app/Http/Controllers/QustionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Qustion;
+use Illuminate\Http\Request;
+
+class QustionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $qustions = Qustion::latest()->paginate(5);
+
+        return view('qustions.index', compact('qustions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Qustion  $qustion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Qustion $qustion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Qustion  $qustion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Qustion $qustion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Qustion  $qustion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Qustion $qustion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Qustion  $qustion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Qustion $qustion)
+    {
+        //
+    }
+}
---
+++ resources/views/qustions/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                    @foreach ($qustions as $qustion)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0">{{$qustion->title}}</h3>
+                                {{str_limit($qustion->body, 250)}}
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                    <div class = "mx-auto">
+                        {{$qustions->links()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
